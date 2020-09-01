92db9c1d8200ceefe0e3fbf368430ae4795bb837
nikolaskokhno <kokhno.mm@gmail.com>
2020-09-01 11:23:50 +0300

    [SERVER]: Create 'GET note by ID' route

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,8 +2,9 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-const { getAllNotes } = require('./routes/notes');
+const { getAllNotes, getNotesByID } = require('./routes/notes');
 
 app.get('/notes', getAllNotes);
+app.get('/notes/:id', getNotesByID);
 
 exports.api = functions.https.onRequest(app);
--- server/functions/routes/notes.js
+++ server/functions/routes/notes.js
@@ -19,7 +19,32 @@ exports.getAllNotes = (req, res) => {
             return res.status(200).json(notes);
         })
         .catch(error => {
-            return res.status(500).json({ message: 'Something went wrong!' });
             console.error(error);
+            return res.status(500).json({ message: 'Something went wrong!' });
+        })
+}
+
+// GET Note by ID
+exports.getNotesByID = (req, res) => {
+    let noteData = {};
+
+    db
+        .doc(`/notes/${req.params.id}`)
+        .get()
+        .then(doc => {
+            if (!doc.exists) {
+                return res.status(404).json({
+                    error: 'Not found', 
+                    message: 'Note with this ID not found!' 
+                });
+            }
+            noteData = doc.data();
+            noteData.id = doc.id;
+            
+            return res.status(200).json(noteData);
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).json({ message: 'Something went wrong!' });
         })
 }
